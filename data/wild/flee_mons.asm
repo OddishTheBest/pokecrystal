7c3b4c0bbebd0eb01b0a320ef3940285beb24cde
OddishTheBest <69400426+OddishTheBest@users.noreply.github.com>
2020-08-10 08:45:36 -0400

    Changed Flee Mons

--- data/wild/flee_mons.asm
+++ data/wild/flee_mons.asm
@@ -2,30 +2,19 @@ FleeMons:
 ; referenced by TryEnemyFlee and FastBallMultiplier
 
 SometimesFleeMons:
-	db MAGNEMITE
-	db GRIMER
-	db TANGELA
-	db MR__MIME
-	db EEVEE
-	db PORYGON
 	db DRATINI
 	db DRAGONAIR
-	db TOGETIC
-	db UMBREON
+	db DRAGONITE
 	db UNOWN
-	db SNUBBULL
-	db HERACROSS
+	db LARVITAR
+	db PUPITAR
+	db TYRANITAR
 	db -1
 
 OftenFleeMons:
-	db CUBONE
 	db ARTICUNO
 	db ZAPDOS
 	db MOLTRES
-	db QUAGSIRE
-	db DELIBIRD
-	db PHANPY
-	db TEDDIURSA
 	db -1
 
 AlwaysFleeMons:
