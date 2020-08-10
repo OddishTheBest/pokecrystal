9f21d06b805b433970f1ba046f995ddb2723655b
OddishTheBest <69400426+OddishTheBest@users.noreply.github.com>
2020-08-10 08:50:15 -0400

    Updated probabilities with new encounter chances

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -4,13 +4,11 @@ mon_prob: MACRO
 ENDM
 
 GrassMonProbTable:
-	mon_prob 30,  0 ; 30% chance
-	mon_prob 60,  1 ; 30% chance
-	mon_prob 80,  2 ; 20% chance
-	mon_prob 90,  3 ; 10% chance
-	mon_prob 95,  4 ;  5% chance
-	mon_prob 99,  5 ;  4% chance
-	mon_prob 100, 6 ;  1% chance
+	mon_prob 20,  0 ; 20% chance
+	mon_prob 40,  0 ; 20% chance
+	mon_prob 60   0 ; 20% chance
+	mon_prob 80   0 ; 20% chance
+	mon_prob 100  0 ; 20% chance
 
 WaterMonProbTable:
 	mon_prob 60,  0 ; 60% chance
